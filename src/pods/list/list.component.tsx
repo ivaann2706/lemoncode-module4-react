ddc6e0d2aed21f7067eaba84cc362343d97143ae
Ivan Alba <ivaann2706@gmail.com>
2022-10-18 00:55:42 +0200

    feat: show the company members in a table with pagination

--- src/pods/list/list.component.tsx
+++ src/pods/list/list.component.tsx
@@ -1,5 +1,12 @@
 import React from "react";
+
 import { Link } from "react-router-dom";
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+import TablePagination from "@mui/material/TablePagination";
 
 import { routes } from "@/core/router/routes";
 import { MemberEntity } from "./list.vm";
@@ -10,21 +17,55 @@ interface Props {
 
 export const List = (props: Props) => {
   const { members } = props;
+  const [page, setPage] = React.useState(0);
+  const [rowsPerPage, setRowsPerPage] = React.useState(5);
+
+  const handleChangePage = (
+    event: React.MouseEvent<HTMLButtonElement, MouseEvent>,
+    pag: number
+  ) => {
+    setPage(pag);
+  };
+
+  const handleChangeRowsPerPage = (e) => {
+    setRowsPerPage(e.target.value);
+  };
 
   return (
     <>
-      <div className="list-user-list-container">
-        <span className="list-header">Avatar</span>
-        <span className="list-header">Id</span>
-        <span className="list-header">Name</span>
-        {members.map((member: MemberEntity) => (
-          <React.Fragment key={member.id}>
-            <img src={member.avatarUrl} />
-            <span>{member.id}</span>
-            <Link to={routes.detail(member.login)}>{member.login}</Link>
-          </React.Fragment>
-        ))}
-      </div>
+      <Table>
+        <TableHead>
+          <TableRow>
+            <TableCell align="center">Avatar</TableCell>
+            <TableCell align="center">Id</TableCell>
+            <TableCell align="center">Name</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {members
+            .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+            .map((member: MemberEntity) => (
+              <TableRow key={member.id}>
+                <TableCell align="center">
+                  <img className="avatar" src={member.avatarUrl} />
+                </TableCell>
+                <TableCell align="center">{member.id}</TableCell>
+                <TableCell align="center">
+                  <Link to={routes.detail(member.login)}>{member.login}</Link>
+                </TableCell>
+              </TableRow>
+            ))}
+        </TableBody>
+      </Table>
+      <TablePagination
+        rowsPerPageOptions={[5, 10, 25]}
+        component="div"
+        count={members.length}
+        rowsPerPage={rowsPerPage}
+        page={page}
+        onPageChange={handleChangePage}
+        onRowsPerPageChange={handleChangeRowsPerPage}
+      />
     </>
   );
 };
