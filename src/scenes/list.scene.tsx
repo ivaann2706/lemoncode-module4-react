37ee3b26317ece5447232a5415c7b5f3e88512a1
Ivan Alba <ivaann2706@gmail.com>
2022-10-17 18:49:01 +0200

    refactor: remove unused code

--- src/scenes/list.scene.tsx
+++ src/scenes/list.scene.tsx
@@ -4,11 +4,6 @@ import { AppLayout } from "@/layouts/app.layout";
 import { ListContainer } from "@/pods/list";
 
 export const ListScene = () => {
-  // URL --> /list?page=4&count=10
-  // const { search } = useLocation();
-  // const params = new URLSearchParams(search);
-  // console.log("PARAM PAGE: ", params.get("page")); // PARAM PAGE: 4
-
   return (
     <AppLayout>
       <ListContainer />
